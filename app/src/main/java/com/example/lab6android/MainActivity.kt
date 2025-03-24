ba8f3f05a7aeb959a1cf73dd78a6da829f694fbd
Temirlan814 <t.temirlan.2004@mail.ru>
2025-03-24 16:23:02 +0500

    something also did

--- app/src/main/java/com/example/lab6android/MainActivity.kt
+++ app/src/main/java/com/example/lab6android/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.lab6android
 
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val imageDetails = getListData()
+        val listView: ListView = findViewById(R.id.listView)
+
+        listView.adapter = CustomListAdapter(this, imageDetails)
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener { parent, _, position, _ ->
+            val country = parent.getItemAtPosition(position) as Country
+            Toast.makeText(
+                this@MainActivity,
+                "Selected: ${country.countryName}",
+                Toast.LENGTH_LONG
+            ).show()
         }
+    }
+    private fun getListData(): List<Country> {
+        val list = mutableListOf<Country>()
+        val vietnam = Country("Vietnam", "vn", 98000000)
+        val usa = Country("USA", "us", 320000000)
+        val russia = Country("Russia", "ru", 142000000)
+
+        list.add(vietnam)
+        list.add(usa)
+        list.add(russia)
+
+        return list
     }
 }
